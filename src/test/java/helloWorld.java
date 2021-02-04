2fada8974c21c1a8381fda9349678f1f8e2d3a38
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 11:03:33 +0300

    hello again

--- src/test/java/helloWorld.java
+++ src/test/java/helloWorld.java
@@ -2,5 +2,6 @@ public class helloWorld {
     public static void main(String[] args) {
         System.out.println("\"hello\" = " + "hello");
         System.out.println("\"hello again\" = " + "hello again");
+        //svv<c<c
     }
 }
