7d639d443171acaf8194439fda1a60e8b55d6618
Fmharmanci <71557439+Fmharmanci@users.noreply.github.com>
2021-02-04 11:03:33 +0300

    hello again

---
+++ src/test/java/helloWorld.java
@@ -0,0 +1,6 @@
+public class helloWorld {
+    public static void main(String[] args) {
+        System.out.println("\"hello\" = " + "hello");
+        System.out.println("\"hello again\" = " + "hello again");
+    }
+}
